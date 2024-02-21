215aa3a3cf602df90aaa09eccc6a5bb9df29dd0a
Luis Chavarriaga <luischa123@gmail.com>
2024-02-20 23:20:04 -0500

    fix carousel styles

--- src/ui/HomeCarousel.tsx
+++ src/ui/HomeCarousel.tsx
@@ -5,15 +5,15 @@ import { Carousel as BaseCarousel } from "flowbite-react";
 const carouselTheme: CustomFlowbiteTheme["carousel"] = {
   "indicators" : {
     "active": {
-      "off": "bg-white/50 hover:bg-white dark:bg-gray-600/50 dark:hover:bg-gray-600",
-      "on": "bg-white dark:bg-gray-600"
+      "off": "bg-black/50 hover:bg-black dark:bg-gray-600/50 dark:hover:bg-gray-600",
+      "on": "bg-black dark:bg-gray-600"
     }
   },
   control: {
     base:
       "inline-flex h-8 w-8 items-center justify-center rounded-full " +
-      "bg-white/30 group-hover:bg-white/50 group-focus:outline-none " +
-      "group-focus:ring-4 group-focus:ring-white dark:bg-gray-600/30 " +
+      "bg-black/30 group-hover:bg-black/50 group-focus:outline-none " +
+      "group-focus:ring-4 group-focus:ring-black dark:bg-gray-600/30 " +
       "dark:group-hover:bg-gray-600/60 " +
       "dark:group-focus:ring-gray-600/70 sm:h-10 sm:w-10",
   },
